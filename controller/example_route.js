891db0248a9aac3fbf57c214aa11f2bbb5c2c9a6
olengs <limjunchen37@gmail.com>
2026-03-05 17:36:23 +0800

    updated example
    example

--- controller/example_route.js
+++ controller/example_route.js
@@ -1,20 +1,20 @@
 const express = require("express");
-const router = express.Router();
+const routers = express.Router();
 
-router.get("/", (req, res) => {
+routers.get("/", (req, res) => {
     res.render("example/example", {test: "testing123"});
 });
 
-router.get("/create", (req, res) => {
+routers.get("/create", (req, res) => {
     res.send("This is the example create homepage")
 });
 
-router.get("/update", (req, res) => {
+routers.get("/update", (req, res) => {
     res.send("This is the example update homepage")
 });
 
-router.get("/delete", (req, res) => {
+routers.get("/delete", (req, res) => {
     res.send("This is the example delete homepage")
 });
 
-module.exports = router;
+module.exports = routers;
