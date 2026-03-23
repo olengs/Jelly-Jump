bb0660d2cc596df49f750260c7a8719ceddbf25c
olengs <limjunchen37@gmail.com>
2026-03-23 16:19:27 +0800

    Emergency update fix for gameview

--- controllers/game_controllers.js
+++ controllers/game_controllers.js
@@ -3,14 +3,14 @@ const recordsModel = require("../models/game-records");
 exports.gameViewController = (req, res) => {
     //add customizable shop etc
     const user = req.session.user;
-    const character = 0;
-    res.render("game/gameview", {UUID: user.UUID, character});
+    const characterID = 0;
+    res.render("game/gameview", {UUID: user.UUID, characterID});
 };
 
 exports.EndGameUpdateController = (req, res) => {
-    let {UUID, highscore, gameEndTime, character} = req.body;
+    let {playerUID, highscore, gameEndTime, character} = req.body;
     let endTime = new Date(gameEndTime);
 
     //update db with user score
-    console.log(`${endTime.toISOString()}: Updating UUID: ${UUID} and highscore: ${highscore} into DB`);
+    console.log(`${endTime.toISOString()}: Updating UUID: ${playerUID} and highscore: ${highscore} with character: ${character} into DB`);
 };
