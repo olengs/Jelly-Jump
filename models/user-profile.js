5ceab07b5245d6e52210ae4d1a3ab491db2cb389
janicetzx <janice.tan.2025@computing.smu.edu.sg>
2026-03-23 15:57:21 +0800

    changes

--- models/user-profile.js
+++ models/user-profile.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
     username: {type: String, required: true}, 
     email: {type: String, required: true}, 
     password: {type: String, required: true},
-    avatar: {type: String, default: ""}, 
+    nickname: {type: String, default: ""}, 
     bio: {type: String, default: ""}
 }); 
 
