e855876681bfe7105fa7dc2611450668b01bb8af
olengs <limjunchen37@gmail.com>
2026-03-23 22:38:10 +0800

    updated user to not use crypto

--- controllers/game_controllers.js
+++ controllers/game_controllers.js
@@ -1,16 +1,19 @@
 const recordsModel = require("../models/game-records");
+const UserModel = require("../models/user-model");
 
 exports.gameViewController = (req, res) => {
     //add customizable shop etc
     const user = req.session.user;
     const characterID = 0;
-    res.render("game/gameview", {UUID: user.UUID, characterID});
+    res.render("game/gameview", {playerId: user._id, characterID});
 };
 
-exports.EndGameUpdateController = (req, res) => {
-    let {playerUID, highscore, gameEndTime, character} = req.body;
+exports.EndGameUpdateController = async (req, res) => {
+    let {playerId, highscore, gameEndTime, character} = req.body;
     let endTime = new Date(gameEndTime);
 
+    const user = await UserModel.getUserById(playerId);
+
     //update db with user score
-    console.log(`${endTime.toISOString()}: Updating UUID: ${playerUID} and highscore: ${highscore} with character: ${character} into DB`);
+    console.log(`${endTime.toISOString()}: Updating uname: ${user.username} and highscore: ${highscore} with character: ${character} into DB`);
 };
