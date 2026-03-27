9433b658349adae64be89a467a91d9f143575910
ikanbilliess <brilynn.tan.2025@computing.smu.edu.sg>
2026-03-27 14:49:20 +0800

    did freindslist , add an d delete and show

--- controllers/friends_controllers.js
+++ controllers/friends_controllers.js
@@ -5,30 +5,42 @@ exports.friendslist = async(req,res) =>{
     try {
 
        const user = await User.findById(req.session.user._id) // looks at the User document and find the id in the bracker
-       const friendslist = user.friends
-       res.render('friends/friends',{friendslist})
+       const friendslist = user.friends || [] // [ { }, { }]
+       res.render('friends/friends',{friendslist,error:''},)
 }catch(error){
     console.log('cannot show ')
 } }
 
 exports.addFriend = async(req,res)=>{
     try{
+
         console.log('req.session.user:', req.session.user);
         if (!req.session.user) {
         return res.status(401).send('You are not logged in');
     }
 
-        const friendname= req.body.friendname
-         await User.findByIdAndUpdate(
+        const friendname= req.body.friendname // what u key in 
+        const user = await User.findById(req.session.user._id) // looks at the User document and find the id in the bracker
+        const friendslist = user.friends // [ { }, { }]
+        
+        const isFriend = friendslist.some(friend => friendname === friend.friendName) // true or false 
+
+        if (isFriend === false){ // not inside
+            
+            await User.findByIdAndUpdate(
             req.session.user._id,
-{
-    $push: {friends: {friendName:friendname}}
-  } 
+                {
+                    $push: {friends: {friendName:friendname}}
+                } )
+            return res.redirect('/friendslist') // redirect cz if not friendslist is not the updated one
+            }
+        else { // inside
+            
+            return res.render('friends/friends',{error:'already exist',friendslist})
+        }
 
-  
-);
-
-         res.redirect('/friendslist')
+        
+         
     } catch (error) {
         console.log(error)
         res.send('error cannot add')
@@ -37,11 +49,21 @@ exports.addFriend = async(req,res)=>{
 
 exports.deleteFriend = async(req,res) =>{
     try{
-        const friendid= req.body.friendid
-        await User.deleteOne({_id:friendid})
-        res.redirect('friendslist')
+        const friendname= req.body.friendname
+        await User.findByIdAndUpdate(
+            req.session.user._id ,// which user document 
+            {
+                $pull:{friends:{friendName:friendname}}
+            }
+        )
+        res.redirect('/friendslist')
     }catch(error){
         console.log(error)
         res.send('error cannot delete') }}
 
 
+
+// for the addfriend. can use for each is can but need to do the isthere= false and isthere= true because foreach will loop through everything . EBERYTHING bfr it ends. thats why use .some can already
+
+// await User.deleteOne({_id: friendid})	  deletes an entire user document
+// $pull	                          deletes one element inside array
