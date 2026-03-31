6573ef63bba726a1067c206d09971a878f147009
ikanbilliess <brilynn.tan.2025@computing.smu.edu.sg>
2026-03-31 11:09:11 +0800

    changed the middelware thing

--- routes/friends-routes.js
+++ routes/friends-routes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const friendControllers = require("../controllers/friends_controllers");
+const middleware = require("../middleware/user-middleware");
 
-router.get("/friendslist",friendControllers.friendslist);
-router.post('/addfriend',friendControllers.addFriend)
-router.post('/deletefriend',friendControllers.deleteFriend)
+router.get("/friendslist",middleware.requireUser, friendControllers.friendslist);
+router.post('/addfriend',middleware.requireUser,friendControllers.addFriend)
+router.post('/deletefriend',middleware.requireUser,friendControllers.deleteFriend)
 
 module.exports = router;
